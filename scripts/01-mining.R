1ad5e1b29250623f32b234411f1e6bde97ce20b9
Annick Robatel <annick.robatel@rega-sense.ch>
2026-03-07 11:56:25 +0100

    initial API request to retrieve MEP data

--- scripts/01-mining.R
+++ scripts/01-mining.R
@@ -1,1 +1,20 @@
+#Getting started
 library(tidyverse)
+library(httr)
+
+#defining the url for the informations about the Members of the Parliment 
+url <- "https://data.europarl.europa.eu/api/v2/meps?limit=50" 
+
+#user-Agent 
+response <- GET(
+  url,
+  add_headers(
+    "User-Agent" = "mep-project-research-1.0",
+    "Accept" = "application/ld+json"
+  )
+)
+
+
+# Check if this works 
+status_code(response)
+

32e70d893459a88abf8e724cb8b730d34290f053
Annick Robatel <annick.robatel@rega-sense.ch>
2026-03-07 14:49:13 +0100

    created df and saved as csv

--- scripts/01-mining.R
+++ scripts/01-mining.R
@@ -3,10 +3,10 @@ library(tidyverse)
 library(httr)
 library(jsonlite)
 
-#defining the url for the informations about the Members of the Parliment 
+#defining the url for the informations about the Members of the Parliment (Promt 1 ChatGPT)
 url <- "https://data.europarl.europa.eu/api/v2/meps?limit=50" 
 
-#user-Agent 
+#user-Agent (Promt 2 ChatGPT)
 response <- GET(
   url,
   add_headers(
@@ -16,7 +16,7 @@ response <- GET(
 )
 
 
-# Check if this works 
+# Check if this works -> 200
 status_code(response)
 
 #saving what i recieved as text
@@ -24,5 +24,13 @@ data <- content(response, as = "text", encoding = "UTF-8")
 
 #Putting the JSON in R
 data_json <- fromJSON(data, flatten = TRUE)
+
+#getting a df 
+meps_df <- as.data.frame(data_json$data)
+glimpse(meps_df)
+
+#save as csv in data_raw
+write_csv(meps_df, "data_raw/meps_raw.csv")
+
 
 

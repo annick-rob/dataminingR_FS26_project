90c5c2783eca602d5f0c217ee4e752080ea757b4
Annick Robatel <annick.robatel@rega-sense.ch>
2026-03-07 12:04:33 +0100

    parse the API response and convert the JSON to an R object

--- scripts/01-mining.R
+++ scripts/01-mining.R
@@ -1,6 +1,7 @@
 #Getting started
 library(tidyverse)
 library(httr)
+library(jsonlite)
 
 #defining the url for the informations about the Members of the Parliment 
 url <- "https://data.europarl.europa.eu/api/v2/meps?limit=50" 
@@ -17,4 +18,11 @@ response <- GET(
 
 # Check if this works 
 status_code(response)
+
+#saving what i recieved as text
+data <- content(response, as = "text", encoding = "UTF-8")
+
+#Putting the JSON in R
+data_json <- fromJSON(data, flatten = TRUE)
+
 

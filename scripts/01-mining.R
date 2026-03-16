a70222f4caed47a4ac705008d38b94f0ad9aa787
Annick Robatel <annick.robatel@rega-sense.ch>
2026-03-16 10:59:03 +0100

    addition to the get request for speeches

--- scripts/01-mining.R
+++ scripts/01-mining.R
@@ -485,7 +485,10 @@ glimpse(meps_df)
 write_csv(meps_df, "data_raw/meps_terms_8_10.csv")
 
 ################################################################################
+
+################
 #speeches per mep (Promt 25-31  ChatGPT)
+################
 
 speech_counts <- list()
 
@@ -557,5 +560,8 @@ for (i in 1:nrow(meps_preprocessed)) {
 }
 
 speech_counts_df <- bind_rows(speech_counts)
-
 write_csv(speech_counts_df, "data_raw/speech_counts_terms_9_10.csv")
+
+
+
+
